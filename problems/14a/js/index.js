e9473a5917b17212d57f3e7311a49ccf130df1e4
Brayzure <rictylp@gmail.com>
2018-12-14 00:28:27 -0500

    Day 14 Javascript solutions

---
+++ problems/14a/js/index.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const INPUT_LOCATION = "../input.txt";
+const rawInput = fs.readFileSync(INPUT_LOCATION, "utf8").trim();
+const input = rawInput.split("\n").map(e => e.trim());
+
+const solution = solve(input);
+console.log(solution);
+
+function solve(input) {
+    let left = recipes = parseInt(input[0]) - 2;
+    let arr = [3, 7];
+    let elf1 = 0;
+    let elf2 = 1;
+    while(left > 0) {
+        let elf1Score = arr[elf1];
+        let elf2Score = arr[elf2];
+        let newScore = elf1Score + elf2Score;
+        let recipes = newScore.toString().split("");
+        arr.push(+recipes[0]);
+        left--;
+        if(recipes[1]) {
+            arr.push(+recipes[1]);
+            left--;
+        }
+        elf1 = (1 + elf1 + elf1Score) % arr.length;
+        elf2 = (1 + elf2 + elf2Score) % arr.length;
+    }
+    let hold = arr.length + left;
+    left += 10;
+    while(left > 0) {
+        let elf1Score = arr[elf1];
+        let elf2Score = arr[elf2];
+        let newScore = elf1Score + elf2Score;
+        let recipes = newScore.toString().split("");
+        arr.push(+recipes[0]);
+        left--;
+        if(recipes[1]) {
+            arr.push(+recipes[1]);
+            left--;
+        }
+        elf1 = (1 + elf1 + elf1Score) % arr.length;
+        elf2 = (1 + elf2 + elf2Score) % arr.length;
+    }
+    return arr.slice(hold, hold+10).join("");
+}
+
+module.exports = solve;
---
+++ problems/14b/js/index.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const INPUT_LOCATION = "../input.txt";
+const rawInput = fs.readFileSync(INPUT_LOCATION, "utf8").trim();
+const input = rawInput.split("\n").map(e => e.trim());
+
+const solution = solve(input);
+console.log(solution);
+
+function solve(input) {
+    let recipes = parseInt(input[0]);
+    let target = recipes.toString().split("").map(e => +e);
+    let arr = [3, 7];
+    let elf1 = 0;
+    let elf2 = 1;
+    let ding = 0;
+    let found;
+    while(!found) {
+        let elf1Score = arr[elf1];
+        let elf2Score = arr[elf2];
+        let newScore = elf1Score + elf2Score;
+        let recipes = newScore.toString().split("");
+        arr.push(+recipes[0]);
+        if(+recipes[0] === target[ding]) {
+            ding++;
+        }
+        else {
+            ding = 0;
+        }
+        if(ding === target.length) found = arr.length;
+        if(recipes[1]) {
+            if(+recipes[1] === target[ding]) {
+                ding++;
+            }
+            else {
+                ding = 0;
+            }
+            if(ding === target.length) found = arr.length;
+            arr.push(+recipes[1]);
+        }
+        elf1 = (1 + elf1 + elf1Score) % arr.length;
+        elf2 = (1 + elf2 + elf2Score) % arr.length;
+        i--;
+    }
+    return found - target.length;
+}
+
+module.exports = solve;
